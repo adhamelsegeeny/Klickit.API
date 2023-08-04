7688a2d308141446b48def9bb7f73f336bfa50b0
Adham Elsegeeny <adham_elsegeeny@icloud.com>
2023-08-04 03:14:47 +0300

    done

---
+++ ProductsAPI/Data/products.sql
@@ -0,0 +1,27 @@
+﻿create table products (
+	id int not null identity(1,1),
+	name varchar(255) not null,
+	price decimal(19,2) not null,
+	primary key (id)
+)
+
+create table Users (
+	username varchar(255) not null,
+	password varchar(255) not null,
+	primary key (username)
+)
+
+insert into Users (username, password) values ('admin', 'admin')
+
+create table requests (
+	id int not null identity(1,1),
+	username varchar(255) not null,
+	product_id int not null,
+	primary key (id),
+	foreign key (username) references Users(username),
+	foreign key (product_id) references products(id)
+)
+
+select * from requests
+
+
